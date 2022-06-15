170cd7ccb068bb81f759b7a1d150381e557cc944
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-06-14 22:08:54 -0500

    🩹 Fixed form error (#9)

--- src/utils/embeded.ts
+++ src/utils/embeded.ts
@@ -6,6 +6,14 @@ export function createEmbeded(
   user: User,
   client: Client
 ): MessageEmbed {
+  let iconURL: string | null | undefined;
+
+  if (client.user?.avatarURL({ format: "png" }) === null) {
+    iconURL = "https://avatars.githubusercontent.com/u/107168679?s=200&v=4";
+  } else {
+    iconURL = client.user?.avatarURL({ format: "png" });
+  }
+
   return new MessageEmbed()
     .setColor("#ffeded")
     .setTitle(title)
@@ -13,6 +21,6 @@ export function createEmbeded(
     .setTimestamp()
     .setFooter({
       text: `${client.user?.tag}`,
-      iconURL: `${client.user?.avatarURL({ format: "png" })}`,
+      iconURL: iconURL as string,
     });
 }
