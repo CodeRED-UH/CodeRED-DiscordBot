88438015d90f1da7f44c00e6a809f59494a1e763
Ben Key <54153434+mellophone@users.noreply.github.com>
2022-06-15 13:27:11 -0500

    Added check-in sheets logging

--- src/commands/checkin.ts
+++ src/commands/checkin.ts
@@ -2,6 +2,7 @@ import { SlashCommandBuilder } from "@discordjs/builders";
 import { Command } from "../interfaces/Command";
 import { createEmbeded } from "../utils/embeded";
 import { google } from "googleapis";
+import { time } from "console";
 
 export const checkin: Command = {
     data: new SlashCommandBuilder()
@@ -14,21 +15,21 @@ export const checkin: Command = {
                 .setRequired(true)
         ),
     run: async (interaction, client) => {
-        await interaction.deferReply({ ephemeral: true });
+        await interaction.deferReply({ ephemeral: false });
         const { user } = interaction;
         const email = interaction.options.getString("email", true);
         var verif = false;
 
         const successMessage = createEmbeded(
-            "**You have successfully checked in!**",
+            "**You have successfully checked in!** ✅",
             `Thank you for checking into CodeRED Odyssey!\nYour email ***${email}*** has been verified!`,
             user,
             client
         ).setColor(0x00FF00);
 
         const failMessage = createEmbeded(
-            "**CHECK-IN FAILED**",
-            `Your email ***${email}*** does not match our records.\nPlease make sure to enter the email you signed up with.`,
+            "**CHECK-IN FAILED** ❌",
+            `The email ***${email}*** does not match our records.\nPlease make sure to enter the email you signed up with.`,
             user,
             client
         ).setColor(0xFF0000);
@@ -44,7 +45,7 @@ export const checkin: Command = {
             const sheetdata = await sheet.spreadsheets.values.get({
                 spreadsheetId: "1jT_gUsH6_E6FYK03Apb57vpBbVgiBW0tMSCmjLsGxxs",
                 auth: auth,
-                range: "Sheet1!A1:B"
+                range: "Sheet1!A1:D"
             })
             // console.log(user.tag);
             // console.log(sheetdata.data.values?.length);
@@ -56,9 +57,33 @@ export const checkin: Command = {
                     if (cellDisc == user.tag) {
                         const cellEmail = sheetdata.data.values?.at(i)?.at(1);
                         if (cellEmail == email) {
+                            verif = true;
+                            console.log(sheetdata.data.values?.at(i)?.at(2));
+                            if (sheetdata.data.values?.at(i)?.at(2) == "Checked In ✅") {
+
+                                const checkedinMessage = createEmbeded(
+                                    "**You are already checked in!** ✋",
+                                    `You have already checked in with ***${email}*** at ***${sheetdata.data.values?.at(i)?.at(3)}***.\nIf this is wrong, please contact a CodeRED officer.`,
+                                    user,
+                                    client
+                                ).setColor(0xFFFF00);
+
+                                console.log("USELESS");
+                                await interaction.editReply({ embeds: [checkedinMessage] });
+                                break;
+                            }
+                            let dateTime = new Date();
+                            await sheet.spreadsheets.values.update({
+                                spreadsheetId: "1jT_gUsH6_E6FYK03Apb57vpBbVgiBW0tMSCmjLsGxxs",
+                                auth: auth,
+                                range: "Sheet1!C" + (i + 1).toString(),
+                                valueInputOption: "RAW",
+                                requestBody: {
+                                    values: [["Checked In ✅", dateTime.toLocaleTimeString() + " " + dateTime.toLocaleDateString()]]
+                                }
+                            })
                             console.log("SUCCESS");
                             await interaction.editReply({ embeds: [successMessage] });
-                            verif = true;
                             break;
                         }
                     }
