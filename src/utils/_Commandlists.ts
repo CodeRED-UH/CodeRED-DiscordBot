6d2df65172a94e662f496e6e0f53cfb0ac79b09c
Bryant Le <80173797+LeftAutomated@users.noreply.github.com>
2022-06-15 23:59:24 -0500

    ✨ Purge command (#14)
    * 🩹 Fixed form error (#9)
    
    * purge command
    
    Co-authored-by: Johnny <44646589+Junnyyy@users.noreply.github.com>

--- src/utils/_Commandlists.ts
+++ src/utils/_Commandlists.ts
@@ -1,6 +1,7 @@
 import { Command } from "../interfaces/Command";
 import { help } from "../commands/help";
 import { report } from "../commands/report";
+import { purge } from "../commands/purge";
 import { map } from "../commands/map";
 
-export const CommandList: Command[] = [help, report, map];
+export const CommandList: Command[] = [help, report, purge, map];
