f84f1a2800cbcb646f2e80a5843fbfb3a1495a06
Mihir Sahu <46336107+MihirSahu@users.noreply.github.com>
2022-06-15 23:54:21 -0500

    Added venue and exit map commands (#15)
    * 🩹 Fixed form error (#9)
    
    * 🩹 Fixed form error (#9)
    
    * 🥅 Added env validation
    
    * 🥅 Added extra handling for undefined URL event
    
    * Circleci project setup (#12)
    
    * Add .circleci/config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * Updated config.yml
    
    * 📝Added readme badges (#13)
    
    📝 Added badges
    
    * Added venue and exit map commands
    
    Co-authored-by: Johnny <44646589+Junnyyy@users.noreply.github.com>

--- src/utils/_Commandlists.ts
+++ src/utils/_Commandlists.ts
@@ -1,5 +1,6 @@
 import { Command } from "../interfaces/Command";
 import { help } from "../commands/help";
 import { report } from "../commands/report";
+import { map } from "../commands/map";
 
-export const CommandList: Command[] = [help, report];
+export const CommandList: Command[] = [help, report, map];
