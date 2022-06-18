7c5495f32db824fde9dd08c17935aaab3152567b
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-06-17 19:44:21 -0500

    ✨Upgraded Embeded to include embeded types

--- src/utils/embeded.ts
+++ src/utils/embeded.ts
@@ -1,11 +1,6 @@
-import { Client, MessageEmbed, User } from "discord.js";
+import { Client, MessageEmbed } from "discord.js";
 
-export function createEmbeded(
-  title: string,
-  message: string,
-  user: User,
-  client: Client
-): MessageEmbed {
+const checkAvatar = (client: Client): string => {
   let iconURL: string | null | undefined;
 
   if (
@@ -17,9 +12,19 @@ export function createEmbeded(
     iconURL = client.user?.avatarURL({ format: "png" });
   }
 
+  return iconURL as string;
+};
+
+export function general(
+  title: string,
+  message: string,
+  client: Client
+): MessageEmbed {
+  const iconURL = checkAvatar(client);
+
   return new MessageEmbed()
     .setColor("#ffeded")
-    .setTitle(title)
+    .setTitle(`${title}`)
     .setDescription(message)
     .setTimestamp()
     .setFooter({
@@ -27,3 +32,93 @@ export function createEmbeded(
       iconURL: iconURL as string,
     });
 }
+
+export function success(
+  title: string | null,
+  message: string,
+  client: Client
+): MessageEmbed {
+  const iconURL = checkAvatar(client);
+
+  if (title) {
+    return new MessageEmbed()
+      .setColor("#4BB543")
+      .setTitle(`✅ ${title}`)
+      .setDescription(message)
+      .setTimestamp()
+      .setFooter({
+        text: `${client.user?.tag}`,
+        iconURL: iconURL as string,
+      });
+  }
+
+  return new MessageEmbed()
+    .setColor("#4BB543")
+    .setTitle(`✅ Success!`)
+    .setDescription(message)
+    .setTimestamp()
+    .setFooter({
+      text: `${client.user?.tag}`,
+      iconURL: iconURL as string,
+    });
+}
+
+export const error = (
+  title: string | null,
+  message: string,
+  client: Client
+): MessageEmbed => {
+  const iconURL = checkAvatar(client);
+
+  if (title) {
+    return new MessageEmbed()
+      .setColor("#FF0000")
+      .setTitle(`❌ ${title}`)
+      .setDescription(message)
+      .setTimestamp()
+      .setFooter({
+        text: `${client.user?.tag}`,
+        iconURL: iconURL as string,
+      });
+  }
+
+  return new MessageEmbed()
+    .setColor("#FF0000")
+    .setTitle("❌ There seems to have been an error.")
+    .setDescription(message)
+    .setTimestamp()
+    .setFooter({
+      text: `${client.user?.tag}`,
+      iconURL: iconURL as string,
+    });
+};
+
+export const warn = (
+  title: string | null,
+  message: string,
+  client: Client
+): MessageEmbed => {
+  const iconURL = checkAvatar(client);
+
+  if (title) {
+    return new MessageEmbed()
+      .setColor("#eed202")
+      .setTitle(`⚠️ ${title}`)
+      .setDescription(message)
+      .setTimestamp()
+      .setFooter({
+        text: `${client.user?.tag}`,
+        iconURL: iconURL as string,
+      });
+  }
+
+  return new MessageEmbed()
+    .setColor("#eed202")
+    .setTitle("⚠️ Warning")
+    .setDescription(message)
+    .setTimestamp()
+    .setFooter({
+      text: `${client.user?.tag}`,
+      iconURL: iconURL as string,
+    });
+};
