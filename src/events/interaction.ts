45820ca6ae1c54f634bf63a408ee31d73657231f
Johnny <44646589+Junnyyy@users.noreply.github.com>
2022-09-08 16:13:44 -0500

    🐛 Fixed bug with ChatInput type

--- src/events/interaction.ts
+++ src/events/interaction.ts
@@ -1,15 +1,33 @@
-import { Client, Interaction } from "discord.js";
+import {
+  Client,
+  ChatInputCommandInteraction,
+  BaseInteraction,
+} from "discord.js";
 import { CommandList } from "../utils/_Commandlists";
+import { error } from "../utils/embededCreator";
 
 export const onInteraction = async (
-  interaction: Interaction,
+  interaction: BaseInteraction,
   client: Client
 ) => {
   if (interaction.isCommand()) {
     for (const Command of CommandList) {
       if (interaction.commandName == Command.data.name) {
-        await Command.run(interaction, client);
-        break;
+        try {
+          await Command.execute(
+            interaction as ChatInputCommandInteraction,
+            client
+          );
+          break;
+        } catch (err) {
+          const errorMessage = error(
+            client,
+            undefined,
+            "An error occured while attempting to run the command.",
+            []
+          );
+          await interaction.reply({ embeds: [errorMessage], ephemeral: true });
+        }
       }
     }
   }
